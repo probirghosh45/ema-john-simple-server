b0c1d45180181d667d35121c5150a7cf05379966
Probir Ghosh <probirghosh@outlook.com>
2021-08-15 17:27:20 +0600

    addOrder API added

--- index.js
+++ index.js
@@ -20,31 +20,32 @@ const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-  const productCollection = client.db("Ema_John_DB").collection("products");
-//   console.log('Database Connected')
+    const productsCollection = client.db("Ema_John_DB").collection("products");
+    const ordersCollection = client.db("Ema_John_DB").collection("orders");
+    //   console.log('Database Connected')
 
   //post endpoint
    app.post('/addProduct',(req,res)=>{
        const products=req.body;
        console.log(products)
-       productCollection.insertMany(products)  //insertOne / insertMany
+       productsCollection.insertOne(products)  //insertOne / insertMany
        .then(result=>{
         //    console.log(result);
-        console.log(result.insertedCount);
+        // console.log(result.insertedCount);
         res.send(result.insertedCount)
           
        })
    })
 
    app.get('/products',(req,res)=>{
-       productCollection.find({})
+       productsCollection.find({})
        .toArray((err,documents)=>{
            res.send(documents);
        })
    })
 
    app.get('/product/:key',(req,res)=>{
-       productCollection.find({key:req.params.key})
+       productsCollection.find({key:req.params.key})
        .toArray((err,documents)=>{
            res.send(documents[0]);
        })
@@ -53,9 +54,19 @@ client.connect(err => {
 
 app.post('/productsByKeys', (req, res) => {
     const productKeys = req.body;
-    productCollection.find({key: { $in: productKeys} })
+    productsCollection.find({key: { $in: productKeys} })
     .toArray( (err, documents) => {
         res.send(documents);
+    })
+})
+
+app.post('/addOrder',(req,res)=>{
+    const order=req.body;
+    console.log(order)
+    ordersCollection.insertOne(order)  //insertOne / insertMany
+    .then(result=>{
+     res.send(result.insertedCount>0)
+       
     })
 })
 
